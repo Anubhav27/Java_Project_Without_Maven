6bdf9d2f2fa7381b915710c87db0cdc570a2c492
Anubhav27 <anubhav.nanda@gmail.com>
2015-06-24 14:28:25 +0530

    first commit

---
+++ src/com/shypz/hello/HelloWorld2.java
@@ -0,0 +1,12 @@
+package com.shypz.hello;
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
+
+	}
+
+}
